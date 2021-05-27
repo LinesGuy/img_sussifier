5158e3e96314cb931788f9394cee52f5ddee464b
LinesGuy <60029482+LinesGuy@users.noreply.github.com>
2021-05-27 09:22:19 +0100

    Add code to remove temp files after completion

--- sus.py
+++ sus.py
@@ -23,6 +23,7 @@
 from PIL import Image
 import numpy as np
 import subprocess
+import os
 
 output_width = 21  # Width of output gif, measured in sussy crewmates
 twerk_frame_count = 6  # 0.png to 5.png
@@ -78,5 +79,10 @@ print("Converting sussy frames to sussy gif")
 # Convert sussied frames to gif. PIL has a built-in method to save gifs but
 # it has dithering which looks sus, so we use ffmpeg with dither=none
 subprocess.call('./ffmpeg -f image2 -i sussified_%d.png -filter_complex "[0:v] scale=sws_dither=none:,split [a][b];[a] palettegen=max_colors=255:stats_mode=single [p];[b][p] paletteuse=dither=none" -r 20 -y -hide_banner -loglevel error sussified.gif')
+
+# Remove temp files
+print("Ejecting temp files from folder")
+for frame_number in range(twerk_frame_count):
+    os.remove(f"sussified_{frame_number}.png")
 
 # lamkas a cute
